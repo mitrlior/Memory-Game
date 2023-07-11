5d71669b7c080bdbc0616727fced9bcee94243da
lior <mitrlior@gmail.com>
2023-07-11 14:57:20 +0300

    fixed import images with gh-pages

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import { useEffect, useState } from "react";
 import SingleCard from "./components/SingleCards";
 
 const cardImages = [
-  { src: "/img/helmet-1.png", matched: false },
-  { src: "/img/potion-1.png", matched: false },
-  { src: "/img/ring-1.png", matched: false },
-  { src: "/img/scroll-1.png", matched: false },
-  { src: "/img/shield-1.png", matched: false },
-  { src: "/img/sword-1.png", matched: false },
+  { src: `${process.env.PUBLIC_URL}/img/helmet-1.png`, matched: false },
+  { src: `${process.env.PUBLIC_URL}/img/potion-1.png`, matched: false },
+  { src: `${process.env.PUBLIC_URL}/img/ring-1.png`, matched: false },
+  { src: `${process.env.PUBLIC_URL}/img/scroll-1.png`, matched: false },
+  { src: `${process.env.PUBLIC_URL}/img/shield-1.png`, matched: false },
+  { src: `${process.env.PUBLIC_URL}/img/sword-1.png`, matched: false },
 ];
 
 const App = () => {
--- src/components/SingleCards.js
+++ src/components/SingleCards.js
@@ -13,7 +13,7 @@ const SingleCard = ({ card, handleChoice, flipped, disabled }) => {
         <img className="front" src={card.src} alt="card front" />
         <img
           className="back"
-          src="/img/cover.png"
+          src={`${process.env.PUBLIC_URL}/img/cover.png`}
           onClick={handleClick}
           alt="card back"
         />
